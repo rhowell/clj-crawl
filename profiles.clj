59fdad4754bcb27aa8ae4c5a80c1d9a0b90b0cf8
Ronnie Howell <rhowell@gracenote.com>
2015-12-22 00:21:22 -0500

    Fixed postgres connection settings

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,10 @@
 {:dev {:env {:frontier-db {:classname "org.postgresql.Driver"
+                           :subprotocol "postgresql"
+                           :subname "//127.0.0.1:5432/clj-crawl-dev"
                            :user "clj-crawler"
-                           :password "purplealigators"
-                           :host "127.0.0.1"
-                           :db "clj-crawl-dev"}}}
+                           :password "purplealigators"}}}
  :test {:env {:frontier-db {:classname "org.postgresql.Driver"
+                            :subprotocol "postgresql"
+                            :subname "//127.0.0.1:5432/clj-crawl-test"
                             :user "clj-crawler"
-                            :password "purplealigators"
-                            :host "127.0.0.1"
-                            :db "clj-crawl-test"}}}}
-
+                            :password "purplealigators"}}}}
