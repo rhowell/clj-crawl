9903a9c5b47ece8089c14f0c8202d8d309a885f4
Ronnie Howell <rhowell@gracenote.com>
2015-12-22 00:08:21 -0500

    Added database create & teardown scripts

---
+++ db/create.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE IF NOT EXISTS urls (
+       id               SERIAL PRIMARY KEY,
+       url              TEXT NOT NULL,
+       priority         INT NOT NULL,
+       crawl_depth      INT NOT NULL,
+       url_protocol     TEXT,
+       url_hostname     TEXT, -- subdomain, domain, and TLD
+       url_port         INT,
+       url_path         TEXT,
+       url_params       TEXT,
+       url_fragment     TEXT,
+       created_at       TIMESTAMP NOT NULL default now(),
+       last_modified    TIMESTAMP NOT NULL default now(),
+       last_crawled_at  TIMESTAMP
+);
+CREATE INDEX urls_url_hostname ON urls(url_hostname);
+-- The same URL with a different fragement, protocol, etc... should be treated the same
+CREATE UNIQUE INDEX urls_uniqueness ON urls(url_hostname, url_path, url_params);
+
